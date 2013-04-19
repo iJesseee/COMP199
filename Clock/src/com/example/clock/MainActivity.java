1e5c4aec26b6270c24ee911cf5f8a42408597437
sinan <sinan@192.168.1.65>
2013-04-19 16:03:29 -0700

    preference menu working strings abstracted into strings.xml

---
+++ Clock/src/com/example/clock/MainActivity.java
@@ -0,0 +1,143 @@
+package com.example.clock;
+
+import java.util.Calendar;
+
+import com.example.clock.R;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends Activity {
+
+	private TextView tvDisplayTime;
+	private TimePicker timePicker1;
+	private Button btnChangeTime;
+
+	private int hour;
+	private int minute;
+
+	static final int TIME_DIALOG_ID = 999;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		setCurrentTimeOnView();
+		addListenerOnButton();
+
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu){
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		switch (item.getItemId()) {
+	    case R.id.about:
+	    	startActivity(new Intent(this, AboutActivity.class));
+	    	return true;
+	    case R.id.action_settings:
+	    	startActivity(new Intent(this, SettingsActivity.class));
+	    	return true;
+	    default:
+	    	return super.onOptionsItemSelected(item);
+		}
+	}
+
+	// display current time
+	public void setCurrentTimeOnView() {
+
+		tvDisplayTime = (TextView) findViewById(R.id.tvTime);
+		timePicker1 = (TimePicker) findViewById(R.id.timePicker1);
+
+		final Calendar c = Calendar.getInstance();
+		hour = c.get(Calendar.HOUR_OF_DAY);
+		minute = c.get(Calendar.MINUTE);
+
+		// set current time into textview
+		tvDisplayTime.setText(new StringBuilder().append(pad(hour)).append(":")
+				.append(pad(minute)));
+
+		// set current time into timepicker
+		timePicker1.setCurrentHour(hour);
+		timePicker1.setCurrentMinute(minute);
+
+	}
+
+	public void addListenerOnButton() {
+
+		btnChangeTime = (Button) findViewById(R.id.btnChangeTime);
+
+		btnChangeTime.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				showDialog(TIME_DIALOG_ID);
+
+			}
+
+		});
+
+	}
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+		case TIME_DIALOG_ID:
+			// set time picker as current time
+			return new TimePickerDialog(this, timePickerListener, hour, minute,
+					false);
+
+		}
+		return null;
+	}
+
+	private TimePickerDialog.OnTimeSetListener timePickerListener = new TimePickerDialog.OnTimeSetListener() {
+		public void onTimeSet(TimePicker view, int selectedHour,
+				int selectedMinute) {
+			hour = selectedHour;
+			minute = selectedMinute;
+			String amOrPm = "";
+			
+			if(hour > 12){
+				hour -= 12;
+				amOrPm = "PM";
+			} else if(hour <= 0){
+				hour += 12;
+				amOrPm = "AM";
+			} else {
+				amOrPm = "AM";
+			}
+			// set current time into textview
+			tvDisplayTime.setText(new StringBuilder().append(pad(hour))
+					.append(":").append(pad(minute)).append(" ").append(amOrPm));
+
+			// set current time into timepicker
+			timePicker1.setCurrentHour(hour);
+			timePicker1.setCurrentMinute(minute);
+
+		}
+	};
+
+	private static String pad(int c) {
+		if (c >= 10)
+			return String.valueOf(c);
+		else
+			return "0" + String.valueOf(c);
+	}
+}
